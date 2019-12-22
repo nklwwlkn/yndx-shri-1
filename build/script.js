9bd25fca441274f828fad8183f57491398a1eb18
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2019-12-22 05:52:02 +0400

    Second auto-test attempt

---
+++ build/script.js
@@ -0,0 +1,38 @@
+/* test-script */
+
+const eventHandler = () => {
+  document.body.addEventListener("click", event => {
+    if (event.target.closest(".history__transaction")) {
+      /* .history__transaction_brand_blue .history__hide */
+      const currentHistoryTransaction = event.target.closest(
+        ".history__transaction"
+      ).classList[1];
+
+      const currentHistoryHide = `${currentHistoryTransaction} .history__hide`;
+
+      if (
+        document.querySelector(
+          `.${event.target.closest(".history__transaction").classList[1]}`
+        ).style.height !== "auto"
+      ) {
+        document.querySelector(
+          `.${event.target.closest(".history__transaction").classList[1]}`
+        ).style.height = "auto";
+
+        document.querySelector(
+          `.${currentHistoryTransaction} .history__hide`
+        ).style.display = "block";
+      } else {
+        document.querySelector(
+          `.${event.target.closest(".history__transaction").classList[1]}`
+        ).style.height = "9.85rem";
+
+        document.querySelector(
+          `.${currentHistoryTransaction} .history__hide`
+        ).style.display = "none";
+      }
+    }
+  });
+};
+
+eventHandler();
