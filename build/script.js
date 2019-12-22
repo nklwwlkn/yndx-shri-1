3310efd848fcd8b6f42614871088a930fc2646ee
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2019-12-23 00:11:24 +0400

    Test

--- build/script.js
+++ build/script.js
@@ -1,6 +1,4 @@
-/* test-script */
-
-const eventHandler = () => {
+document.addEventListener("DOMContentLoaded", function() {
   document.body.addEventListener("click", event => {
     if (event.target.closest(".history__transaction")) {
       /* .history__transaction_brand_blue .history__hide */
@@ -33,6 +31,4 @@ const eventHandler = () => {
       }
     }
   });
-};
-
-eventHandler();
+});
